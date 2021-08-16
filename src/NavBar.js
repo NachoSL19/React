fcde3308f1950d1a6e1ba84ba013e6f26678ff04
Ignacio Lavolpe <lavolpe.ignacio@gmail.com>
2021-08-16 05:55:50 -0300

    Actualizacion del proyecto

---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+
+    return (
+        <header id="main-header">
+            <h1>Bienvenido</h1>
+            <nav>
+                <a href="#">Inicio </a>
+                <a href="#">Nosotros</a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
